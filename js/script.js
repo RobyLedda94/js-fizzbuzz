10e85e654e7350c7ad4e44263ba3164762dac4f3
RobyLedda94 <robyledda01@yahoo.it>
2024-06-27 15:34:39 +0200

    [UPD] creata condizione numeri multipli di 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 // Apriamo il ciclo for
 
 for (let i = 1; i <=100; i++){
-    console.log(i) 
+    console.log(i)
+    
+    // creo la condizione per determinare se i numeri sono multipli di 3
+if (i % 3 === 0) {
+    console.log('Frizz');
+}    
+
+
+
 
 }
