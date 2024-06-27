2ebafdba6d3dc3fd6faa9b258454ffadc15d4cf5
RobyLedda94 <robyledda01@yahoo.it>
2024-06-27 15:50:12 +0200

    [UPD]corretta la traccia, e modificato il il messaggio in console

--- js/script.js
+++ js/script.js
@@ -5,10 +5,16 @@ for (let i = 1; i <=100; i++){
     
     // creo la condizione per determinare se i numeri sono multipli di 3
 if (i % 3 === 0) {
-    console.log('Frizz');
+    console.log (`${i} Fizz`);
+
+    // creo la condizione per determinare che i numeri siano multipli di 5
+} else if (i % 5 === 0) {
+    console.log (`${i} Buzz`);
 }    
 
 
 
 
 }
+
+
