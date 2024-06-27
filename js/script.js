8aef1956304cc0a1d2872a2a3ebcb8e90805c4cb
RobyLedda94 <robyledda01@yahoo.it>
2024-06-27 16:18:31 +0200

    [FIX] il blocco condizionale con operatore AND e stato messo come prima condizione

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
 // Apriamo il ciclo for
 
 for (let i = 1; i <=100; i++){
-    console.log(i)
     
+    
+    // creo la condizione con l'operatore AND cosi combino 2 condizioni 
+if (i % 3 === 0 && i % 5=== 0) {
+        console.log (`${i} FizzBuzz`);
     // creo la condizione per determinare se i numeri sono multipli di 3
-if (i % 3 === 0) {
+} else if (i % 3 === 0) {
     console.log (`${i} Fizz`);
 
     // creo la condizione per determinare che i numeri siano multipli di 5
 } else if (i % 5 === 0) {
     console.log (`${i} Buzz`);
 
-    // creo la condizione con l'operatore AND cosi combino 2 condizioni 
-} else if (i % 3 === 0 && i % 5=== 0) {
-    console.log (`${i} FizzBuzz`);
+    
+} else {
+    console.log (i);
+}
 
 
-}  
 
-
+  
 }
 
 
